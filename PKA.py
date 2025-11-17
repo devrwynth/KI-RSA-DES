91386117f41900498b9375b028a8e10ad4721d01
Muhammad Azka Andrian Kusno <162030751+devrwynth@users.noreply.github.com>
2025-11-18 03:01:07 +0700

    Update PKA.py
    Updated key registration to store both public key components.

--- PKA.py
+++ PKA.py
@@ -27,12 +27,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     if not data:
                         break
                     message = data.decode('utf-8')
-                    request_type, id, public_key = message.split(',')
+                    request_type, id, public_key_e, public_key_n = message.split(',')
                     
                     if request_type == 'new':
                         if id in key_database:
                             print(f"Public key with id:{id} has already been registered, overwriting...")
-                        key_database.update({id: public_key})
+                        key_database.update({id: f"{public_key_e},{public_key_n}"})
                         conn.sendall("Public key added".encode('utf-8'))
                     elif request_type == 'request':
                         if id in key_database:
